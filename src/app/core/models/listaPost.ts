588e795e5213264aef105ec77197c8a83ab8973c
ALEXIS JAVIER VERBA <alexisverba.1995@gmail.com>
2023-06-06 15:01:28 -0300

    se agrego metodo q pega a post

---
+++ src/app/core/models/listaPost.ts
@@ -0,0 +1,11 @@
+export interface ListaPost {
+  latitudUbicacion: number;
+  longitudUbicacion: number;
+  distancia: number;
+  comidas: number[];
+  bebidas: number[];
+  marcasComida: string[];
+  marcasBebida: string[];
+  cantidadInvitados: number;
+  presupuesto: number;
+}
