eda0e417b7598789d2dccfbae0dbcd4de3609ec6
carlanahirgaleano <carlanahirgaleano20@gmail.com>
2023-06-13 18:16:14 -0300

    se agregaron lso botones pero no funciona la logicaaaaaaaaaaa

--- src/app/shared/components/card-oferta/card-oferta.component.ts
+++ src/app/shared/components/card-oferta/card-oferta.component.ts
@@ -27,10 +27,16 @@ export class CardOfertaComponent {
   idProducto!: number;
   @Input() oferta!: Oferta;
   @Output() ofertaSeleccionadaActual: EventEmitter<{ oferta: Oferta, index: number }> = new EventEmitter<{ oferta: Oferta, index: number }>();
+  @Input()
+  mostrarBoton=true;
+
 
   confirmarCambio(): void {
     const data = { oferta: this.oferta, index: this.i };
     this.ofertaSeleccionadaActual.emit(data);
+  }
+  public setMostrarBoton(valor: boolean): void {
+    this.mostrarBoton = valor;
   }
 
   
