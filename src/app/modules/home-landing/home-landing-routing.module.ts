faac35b41c39dcc09115802110cb58a10ab7503c
ALEXIS JAVIER VERBA <alexisverba.1995@gmail.com>
2023-06-14 16:31:57 -0300

    rutas

--- src/app/modules/home-landing/home-landing-routing.module.ts
+++ src/app/modules/home-landing/home-landing-routing.module.ts
@@ -1,39 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ConsultaEventoComponent } from '@modules/consulta-evento/page/consulta-evento.component';
-import { FormRegistroComponent } from '@modules/form-registro/form-registro.component';
-import { OptimizadorListaComponent } from '@modules/optimizador-lista/page/optimizador-lista.component';
-import { PageLoginComponent } from '@modules/page-login/page-login.component';
 
 
-const routes: Routes = [
-  {
-    path: '', //TODO (Public) Login, Register, Forgot...
-    component: HomeLandingComponent,
-    loadChildren: () => import('@modules/home-landing/home-landing.module').then(m => m.HomeLandingModule),
-
-  },
-  {
-    path: 'consulta-evento',
-    component: ConsultaEventoComponent,
-    loadChildren: () => import('@modules/consulta-evento/consulta-evento.module').then(m => m.ConsultaEventoModule)
-  },
-  {
-    path: 'optimizador-lista',
-    component: OptimizadorListaComponent,
-    loadChildren: () => import('@modules/optimizador-lista/optimizador-lista.module').then(m => m.OptimizadorListaModule)
-  },
-  {
-    path: 'form-registro',
-    component: FormRegistroComponent,
-    
-  },
-  {
-    path: 'login',
-    component: PageLoginComponent,
-    
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
