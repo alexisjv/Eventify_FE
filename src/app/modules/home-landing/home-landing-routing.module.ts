bb3e76dce33c73be9105065896f9af713eb79cd4
ALEXIS JAVIER VERBA <alexisverba.1995@gmail.com>
2023-06-19 15:54:32 -0300

    mas links

--- src/app/modules/home-landing/home-landing-routing.module.ts
+++ src/app/modules/home-landing/home-landing-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConsultaEventoComponent } from '@modules/consulta-evento/page/consulta-evento.component';
 
 
 const routes: Routes = [
+  
+  {
+    path: 'consulta-evento',
+    component: ConsultaEventoComponent,
+    loadChildren: () => import('@modules/consulta-evento/consulta-evento.module').then(m => m.ConsultaEventoModule)
+  },
 
 ];
 
