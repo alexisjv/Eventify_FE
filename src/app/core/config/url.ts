c4f82f472503be29f53ec68392cabf0d9011300a
alexisjv <42984026+alexisjv@users.noreply.github.com>
2023-07-07 17:38:30 -0300

    Update url.ts

--- src/app/core/config/url.ts
+++ src/app/core/config/url.ts
@@ -1,5 +1,5 @@
 //URL BASE API
 
-//export const API_URL = 'https://eventify-api.azurewebsites.net/api/';
+export const API_URL = 'https://eventify-api.azurewebsites.net/api/';
 
-export const API_URL = 'https://localhost:7292/api/';
+//export const API_URL = 'https://localhost:7292/api/';
